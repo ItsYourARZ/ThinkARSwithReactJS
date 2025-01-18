ac8af3744404c66a58e3e09467381ade7960ed32
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 16:51:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 import "./index.css";
 
 const App = () => {
