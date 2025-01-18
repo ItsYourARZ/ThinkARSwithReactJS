4405340e38e5c1a438856cf3879c4c0e20940d42
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 15:12:23 +0530

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <a href="#" className="logo">Anirudh</a>
+        <nav>
+          <a href="#" className="active">Home</a>
+          <a href="#">Services</a>
+          <a href="#">Skills</a>
+          <a href="#">Education</a>
+          <a href="#">Experience</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+      <section className="home">
+        <div className="home-img">
+        <img src="https://avatars.githubusercontent.com/u/187540760?v=4" alt="Anirudh"/>
+        </div>
+        <div className="home-content">
+          <h1>
+            Hi, I'm <span>Anirudh</span>
+          </h1>
+          <h3 className="typing-text">I'm a <span></span></h3>
+          <p>Currently in Development</p>
+          <p>
+            I'm a passionate web developer who loves creating beautiful and
+            functional websites.
+          </p>
+          <p>I enjoy working with new technologies.</p>
+          <p>
+            Building impactful solutions that make a difference is what drives
+            me every day.
+          </p>
+          <div className="social-icons">
+            <a href="https://discord.gg/8c7p2eJfKy">
+              <i className="fa-brands fa-discord"></i>
+            </a>
+            <a href="https://github.com/ItsYourARZ">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://open.spotify.com/user/31fqmjj657zn7lrqcb7b22a5y7tu?si=dc1d0ba0c0fb493a">
+              <i className="fa-brands fa-spotify"></i>
+            </a>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
