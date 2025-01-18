af7b114824d7d17b7f72d99df627578b7b853c62
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 17:32:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import React from "react";
-import Header from "./components/Header";
-import Home from "./components/Home";
 import "./index.css";
+import profileImage from "https://avatars.githubusercontent.com/u/187540760?v=4"; // Put your image in the assets folder
+import "./components/Home.css";
+import "./components/Header.css";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <section className="home">
+      <header>
+      <a href="#" className="logo">
+        Anirudh
+      </a>
+      <nav className={menuActive ? "active" : ""}>
+        <a href="#" className="active">
+          Home
+        </a>
+        <a href="#">Services</a>
+        <a href="#">Skills</a>
+        <a href="#">Education</a>
+        <a href="#">Experience</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+    </header>
+          <div className="home-img">
+            <img src={profileImage} alt="Anirudh" />
+          </div>
+          <div className="home-content">
+            <h1>
+              Hi, I'm <span>Anirudh</span>
+            </h1>
+            <h3 className="typing-text">
+              I'm a <span></span>
+            </h3>
+            <p>Currently in Development</p>
+            <p>
+              I'm a passionate web developer who loves creating beautiful and
+              functional websites.
+            </p>
+            <p>I enjoy working with new technologies.</p>
+            <p>
+              Building impactful solutions that make a difference is what drives me
+              every day.
+            </p>
+            <div className="social-icons">
+              <a href="https://discord.gg/8c7p2eJfKy">
+                <i className="fa-brands fa-discord"></i>
+              </a>
+              <a href="https://github.com/ItsYourARZ">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="https://open.spotify.com/user/31fqmjj657zn7lrqcb7b22a5y7tu?si=dc1d0ba0c0fb493a">
+                <i className="fa-brands fa-spotify"></i>
+              </a>
+            </div>
+          </div>
+        </section>
   );
 };
 
