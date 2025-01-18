4327c01373caa9e1ca5455b476595679adab7e22
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 17:42:08 +0530

    Reverting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,35 @@
-import React from "react";
-import "./index.css";
-import profileImage from "https://avatars.githubusercontent.com/u/187540760?v=4"; // Put your image in the assets folder
-import "./components/Home.css";
-import "./components/Header.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <section className="home">
-      <header>
-      <a href="#" className="logo">
-        Anirudh
-      </a>
-      <nav className={menuActive ? "active" : ""}>
-        <a href="#" className="active">
-          Home
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="#">Services</a>
-        <a href="#">Skills</a>
-        <a href="#">Education</a>
-        <a href="#">Experience</a>
-        <a href="#">Contact</a>
-      </nav>
-      <div className="hamburger" onClick={toggleMenu}>
-        ☰
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </header>
-          <div className="home-img">
-            <img src={profileImage} alt="Anirudh" />
-          </div>
-          <div className="home-content">
-            <h1>
-              Hi, I'm <span>Anirudh</span>
-            </h1>
-            <h3 className="typing-text">
-              I'm a <span></span>
-            </h3>
-            <p>Currently in Development</p>
-            <p>
-              I'm a passionate web developer who loves creating beautiful and
-              functional websites.
-            </p>
-            <p>I enjoy working with new technologies.</p>
-            <p>
-              Building impactful solutions that make a difference is what drives me
-              every day.
-            </p>
-            <div className="social-icons">
-              <a href="https://discord.gg/8c7p2eJfKy">
-                <i className="fa-brands fa-discord"></i>
-              </a>
-              <a href="https://github.com/ItsYourARZ">
-                <i className="fa-brands fa-github"></i>
-              </a>
-              <a href="https://open.spotify.com/user/31fqmjj657zn7lrqcb7b22a5y7tu?si=dc1d0ba0c0fb493a">
-                <i className="fa-brands fa-spotify"></i>
-              </a>
-            </div>
-          </div>
-        </section>
-  );
-};
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
