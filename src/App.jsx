da73bb84274d67a2726b1c3269ba7706cd0b7c0f
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 18:04:15 +0530

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,29 @@ function App() {
             <a href="https://open.spotify.com/user/31fqmjj657zn7lrqcb7b22a5y7tu?si=dc1d0ba0c0fb493a"><i className="fa-brands fa-spotify"></i></a>
           </div>
         </div>
+
+        <section class="skills">
+        <div class="skill-card">
+            <div class="icon"><i class="fa fa-code"></i></div>
+            <h3>Web Development</h3>
+            <p>I build full-stack web applications using the MERN stack, which includes MongoDB, Express.js, React.js, and Node.js, handling everything from the database to the user interface.</p>
+        </div>
+        <div class="skill-card">
+            <div class="icon"><i class="fa fa-database"></i></div>
+            <h3>Backend Development</h3>
+            <p>Expert at server-side logic, database management, API creation, and ensuring smooth integration between frontend and backend systems, while optimizing performance, security, and scalability.</p>
+        </div>
+        <div class="skill-card">
+            <div class="icon"><i class="fa fa-pencil-alt"></i></div>
+            <h3>UI Design</h3>
+            <p>I'm skilled in UI design, with a focus on creating intuitive, visually appealing interfaces. I ensure a seamless user experience by balancing aesthetics with functionality, optimizing design for responsiveness and usability.</p>
+        </div>
+        <div class="skill-card">
+            <div class="icon"><i class="fa fa-paint-brush"></i></div>
+            <h3>Painting</h3>
+            <p>I'm an adept painter, skilled in various techniques and mediums. I express creativity through vibrant compositions, focusing on detail, emotion, and storytelling to create visually impactful and thought-provoking artworks.</p>
+        </div>
+        </section>
       </section>
     </div>
   );
