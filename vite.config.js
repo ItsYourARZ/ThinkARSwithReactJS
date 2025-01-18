9ddae959bb886b7ca00a429a8704b1cb6b7495b9
It'sYourARZ <anirraj2022@gmail.com>
2025-01-18 16:07:40 +0530

    Reverting

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Ensure the root is set correctly
-  build: {
-    outDir: 'dist', // The folder to output the build files
-  },
-  server: {
-    open: true, // Automatically open the app in the default browser on server start
-  },
-});
+})
