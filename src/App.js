8ff536e5eb840a6cf0f548c2e9a3f7824fa37fab
It'sYourARZ <anirraj2022@gmail.com>
2025-01-20 15:06:07 +0530

    Modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'; // This will include your styles
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href="#" className="logo"></a>
+        <nav>
+          <a href="#" className="active">Home</a>
+          <a href="#">Services</a>
+          <a href="#">Skills</a>
+          <a href="#">Education</a>
+          <a href="#">Experience</a>
+          <a href="#">Contact</a>
+        </nav>
       </header>
+      <section className="home">
+        <div className="home-content">
+          <h1>Hi, I'm <span>Anirudh</span></h1>
+          <h3 className="typing-text">I'm a <span></span></h3>
+          <p>Currently in Development</p>
+          <p>I'm a passionate web developer who loves creating beautiful and functional websites.</p>
+          <p>I enjoy working with new technologies.</p>
+          <p>Building impactful solutions that make a difference is what drives me every day.</p>
+          <div className="social-icons">
+            <a href="https://discord.gg/8c7p2eJfKy"><i className="fa-brands fa-discord"></i></a>
+            <a href="https://github.com/ItsYourARZ"><i className="fa-brands fa-github"></i></a>
+            <a href="https://open.spotify.com/user/31fqmjj657zn7lrqcb7b22a5y7tu?si=dc1d0ba0c0fb493a"><i className="fa-brands fa-spotify"></i></a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
